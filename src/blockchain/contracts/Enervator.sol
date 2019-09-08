89a81f631fc40a176a1d7d1b315aae1d195eccaf
Steve Huckle <steve.huckle@gmail.com>
2019-09-08 16:54:28 +0100

    Struggling to get bignumbers working in .js

--- src/blockchain/contracts/Enervator.sol
+++ src/blockchain/contracts/Enervator.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.7;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
@@ -7,69 +8,108 @@ import "./ABDKMath64x64.sol";
 
 contract Enervator is ERC777, Ownable {
 
-    uint256 constant private initialSupply = 7727623693;
+    struct TokenValues {
+      uint256 supply;
+      int128 pricePerMWh;
+      int128 currentTPES;
+      int128 oldTPES;
+      int128 perCapitaEnergy;
+      int256 unitValue;
+    }
 
-    int128 private pricePerMWh;
-    int128 private currentTPES;
-    int128 private oldTPES;
-    int128 private perCapitaEnergy;
+    TokenValues private values;
 
-    constructor( address[] memory _defaultOperators ) ERC777 ( "Enervator", "EOR", _defaultOperators ) public
+    constructor(
+      TokenValues memory _values,
+      address[] memory _defaultOperators ) ERC777 ( "Enervator", "EOR", _defaultOperators ) public
     {
-      _mint(msg.sender, msg.sender, initialSupply, "", "");
+      require (_values.supply > 0  &&
+               _values.pricePerMWh > 0 &&
+               _values.currentTPES > 0 &&
+               _values.oldTPES > 0 &&
+               _values.perCapitaEnergy > 0);
 
-      pricePerMWh = 100 * 2**64;
-      currentTPES = 162494360000 * 2**64;
-      oldTPES = currentTPES;
-      perCapitaEnergy = 0;
+      values.supply = _values.supply;
+      values.pricePerMWh = _values.pricePerMWh;
+      values.currentTPES = _values.currentTPES;
+      values.oldTPES = _values.oldTPES;
+      values.perCapitaEnergy = _values.perCapitaEnergy;
+
+      _setUnitValue();
+      _mint(msg.sender, msg.sender, values.supply, "", "");
+    }
+
+    function _setUnitValue () private
+    {
+      require(
+        values.perCapitaEnergy > 0 &&
+        values.currentTPES > 0 &&
+        values.oldTPES > 0 &&
+        values.pricePerMWh > 0
+      );
+
+      int128 TPESFactor = ABDKMath64x64.div(values.oldTPES, values.currentTPES);
+      int128 prePrice =  ABDKMath64x64.div(TPESFactor, values.perCapitaEnergy);
+      values.unitValue = ABDKMath64x64.mul(prePrice, values.pricePerMWh);
     }
 
     function setNewTPES ( int128  _amount ) public onlyOwner
     {
       require( _amount > 0 );
 
-      oldTPES = currentTPES;
-      currentTPES = _amount;
+      values.oldTPES = values.currentTPES;
+      values.currentTPES = _amount;
+      _setUnitValue();
     }
 
     function setPerCapitaEnergy ( int128 _amount ) public onlyOwner
     {
       require( _amount > 0 );
 
-      perCapitaEnergy = _amount;
+      values.perCapitaEnergy = _amount;
+      _setUnitValue();
+    }
+
+    function setSupply ( uint256 _amount ) public onlyOwner
+    {
+      require( _amount > 0 );
+
+      uint256 difference = 0;
+      if ( _amount != values.supply ) {
+        if ( _amount < values.supply ) {
+          difference = values.supply - _amount;
+          //burn
+        } else {
+          difference = _amount - values.supply;
+          _mint(msg.sender, msg.sender, difference, "", "");
+        }
+      }
+      values.supply = _amount;
+      _setUnitValue();
     }
 
     function getPricePerMWh () public view returns ( int128 )
     {
-        return pricePerMWh;
+        return values.pricePerMWh;
     }
 
     function getCurrentTPES () public view returns ( int128 )
     {
-        return currentTPES;
+        return values.currentTPES;
     }
 
     function getOldTPES () public view returns ( int128 )
     {
-        return oldTPES;
+        return values.oldTPES;
     }
 
     function getPerCapitaEnergy () public view returns ( int128 )
     {
-        return perCapitaEnergy;
+        return values.perCapitaEnergy;
     }
 
     function getUnitValue () public view returns ( int256 )
     {
-      require(
-        perCapitaEnergy > 0 &&
-        currentTPES > 0 &&
-        oldTPES > 0 &&
-        pricePerMWh > 0
-      );
-
-      int128 TPESFactor = ABDKMath64x64.div(oldTPES, currentTPES);
-      int128 prePrice =  ABDKMath64x64.div(TPESFactor, perCapitaEnergy);
-      return ABDKMath64x64.mul(prePrice, pricePerMWh);
+      return values.unitValue;
     }
 }
